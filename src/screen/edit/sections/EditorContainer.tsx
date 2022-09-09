b62b6c1b71482a8fd2817cf17c02a87d1f2b624d
Ruggero Panzeri <ruggero.panzeri95@gmail.com>
2022-09-09 11:39:08 +0200

    new section

---
+++ src/screen/edit/sections/EditorContainer.tsx
@@ -0,0 +1,7 @@
+export const EditorContainer = ()=>{
+    return (
+        <div class="flex editor-height flex-wrap p-2 rounded-[20px] bg-white">
+            afasd daslkd sladk òalk sòalkd sld òak
+        </div>
+    )
+}
