7541d97eca03b157d2464d0966970edafbd6fff0
Ruggero Panzeri <ruggero.panzeri95@gmail.com>
2022-10-16 21:27:36 +0200

    icona editor

--- src/components/EditorIcon.tsx
+++ src/components/EditorIcon.tsx
@@ -3,7 +3,7 @@ export enum ButtonIconDimension{
     SMALL='small',
     BIG='big'
 }
-export const ButtonIcon:Component<{dimension:ButtonIconDimension, hoverColor:string, hoverBgColor:string,iconName:string,color:string,iconColor:string}> = (props)=>{
+export const EditorIcon:Component<{dimension:ButtonIconDimension, iconName:string}> = (props)=>{
    
 
     function getIcon(dim:ButtonIconDimension, iconName:string){
@@ -12,7 +12,13 @@ export const ButtonIcon:Component<{dimension:ButtonIconDimension, hoverColor:str
             "bin": <svg class={dimension} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>,
             "edit": <svg class={dimension} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>,
             "plus": <svg class={dimension} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>,
-            "del": <svg class={dimension} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+            "del": <svg class={dimension} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>,
+            "italic":<svg xmlns="http://www.w3.org/2000/svg" class={dimension} fill="currentColor"  viewBox="0 0 16 16"><path d="M7.991 11.674 9.53 4.455c.123-.595.246-.71 1.347-.807l.11-.52H7.211l-.11.52c1.06.096 1.128.212 1.005.807L6.57 11.674c-.123.595-.246.71-1.346.806l-.11.52h3.774l.11-.52c-1.06-.095-1.129-.211-1.006-.806z"/></svg>,
+            "bold":<svg xmlns="http://www.w3.org/2000/svg" class={dimension} fill="currentColor"  viewBox="0 0 16 16"><path d="M8.21 13c2.106 0 3.412-1.087 3.412-2.823 0-1.306-.984-2.283-2.324-2.386v-.055a2.176 2.176 0 0 0 1.852-2.14c0-1.51-1.162-2.46-3.014-2.46H3.843V13H8.21zM5.908 4.674h1.696c.963 0 1.517.451 1.517 1.244 0 .834-.629 1.32-1.73 1.32H5.908V4.673zm0 6.788V8.598h1.73c1.217 0 1.88.492 1.88 1.415 0 .943-.643 1.449-1.832 1.449H5.907z"/></svg>,
+            "h1":<svg xmlns="http://www.w3.org/2000/svg" class={dimension} fill="currentColor" viewBox="0 0 16 16"><path d="M8.637 13V3.669H7.379V7.62H2.758V3.67H1.5V13h1.258V8.728h4.62V13h1.259zm5.329 0V3.669h-1.244L10.5 5.316v1.265l2.16-1.565h.062V13h1.244z"/></svg>,
+            "h2":<svg xmlns="http://www.w3.org/2000/svg" class={dimension} fill="currentColor"  viewBox="0 0 16 16"><path d="M7.638 13V3.669H6.38V7.62H1.759V3.67H.5V13h1.258V8.728h4.62V13h1.259zm3.022-6.733v-.048c0-.889.63-1.668 1.716-1.668.957 0 1.675.608 1.675 1.572 0 .855-.554 1.504-1.067 2.085l-3.513 3.999V13H15.5v-1.094h-4.245v-.075l2.481-2.844c.875-.998 1.586-1.784 1.586-2.953 0-1.463-1.155-2.556-2.919-2.556-1.941 0-2.966 1.326-2.966 2.74v.049h1.223z"/></svg>,
+            "h3":<svg xmlns="http://www.w3.org/2000/svg"class={dimension} fill="currentColor"  viewBox="0 0 16 16"><path d="M7.637 13V3.669H6.379V7.62H1.758V3.67H.5V13h1.258V8.728h4.62V13h1.259zm3.625-4.272h1.018c1.142 0 1.935.67 1.949 1.674.013 1.005-.78 1.737-2.01 1.73-1.08-.007-1.853-.588-1.935-1.32H9.108c.069 1.327 1.224 2.386 3.083 2.386 1.935 0 3.343-1.155 3.309-2.789-.027-1.51-1.251-2.16-2.037-2.249v-.068c.704-.123 1.764-.91 1.723-2.229-.035-1.353-1.176-2.4-2.954-2.385-1.873.006-2.857 1.162-2.898 2.358h1.196c.062-.69.711-1.299 1.696-1.299.998 0 1.695.622 1.695 1.525.007.922-.718 1.592-1.695 1.592h-.964v1.074z"/></svg>,
+
         }
         const icon = icons[iconName];    
         if(!icon){
@@ -29,7 +35,7 @@ export const ButtonIcon:Component<{dimension:ButtonIconDimension, hoverColor:str
         return ''
     }
     return (
-        <button style={{"--ib-bg-hover":props.hoverBgColor,"--ib-text-hover":props.hoverColor}} class={"m-1 icon-button "+props.color+" "+props.iconColor+" "+getClass(props.dimension)}>
+        <button class={"m-1 icon-button border-0 hover:border-gray-100 bg-white hover:bg-gray-100 "+getClass(props.dimension)}>
             {getIcon(props.dimension, props.iconName)}
         </button>
     )
