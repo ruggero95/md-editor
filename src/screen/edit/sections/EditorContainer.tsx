50682152d8eb73b59eac7f9be67e0bd38a57f792
Ruggero Panzeri <ruggero.panzeri95@gmail.com>
2022-10-16 21:29:20 +0200

    editor ui

--- src/screen/edit/sections/EditorContainer.tsx
+++ src/screen/edit/sections/EditorContainer.tsx
@@ -1,7 +1,16 @@
+import { ButtonIconDimension } from "../../../components/ButtonIcon"
+import { EditorIcon } from "../../../components/EditorIcon"
+
 export const EditorContainer = ()=>{
     return (
         <div class="flex editor-height flex-wrap p-2 rounded-[20px] bg-white">
-            afasd daslkd sladk òalk sòalkd sld òak
+            <div class="w-full h-12">
+                <EditorIcon dimension={ButtonIconDimension.BIG} iconName="italic"/>
+                <EditorIcon dimension={ButtonIconDimension.BIG} iconName="bold"/>
+            </div>
+            <textarea class="mx-2 editor-textarea border-none active:border-none focus:border-none w-full h-full" >
+                yo
+            </textarea>
         </div>
     )
 }
