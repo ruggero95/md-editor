7dac1072854266469a7fec3ca9c2be2d055d324a
Ruggero Panzeri <ruggero.panzeri95@gmail.com>
2022-10-19 23:39:37 +0200

    db schema collection

---
+++ src/app/db/schema.ts
@@ -0,0 +1,45 @@
+import { RxSchema } from "rxdb";
+
+export const fileschema = {
+    title:'fileschema',
+    version:0,
+    type:'object',
+    primaryKey:{
+        key: 'id',
+        fields: [
+            "creation"
+        ],
+        separator: '-'
+    },
+    properties:{
+        id:{
+            type:'string',
+            maxLength:250
+        },
+        name:{
+            type:'string',
+            maxLegth:500
+        },
+        creation:{
+            type:'string',
+            maxLength:150
+        },
+        lastUpdate:{
+            type:'string'
+        },
+        opened:{
+            type:'string'
+        },
+        editing:{
+            type:'string'
+        },
+        active:{
+            type:'string'
+        },
+        content:{
+            type:'string'
+        }          
+    },
+    required: ['name', 'creation'],
+    indexes:['name','creation']
+}
