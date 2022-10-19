e67c961b4b7d1f6a14d4aef1d5f697df7accd8e2
Ruggero Panzeri <ruggero.panzeri95@gmail.com>
2022-10-19 15:17:34 +0200

    editor pills and content editor

--- src/screen/edit/sections/FileMainRightSide.tsx
+++ src/screen/edit/sections/FileMainRightSide.tsx
@@ -1,32 +1,21 @@
+import { files, FileStored } from "../../../app/files/files.store"
 import { FileEditorPills } from "../../../components/FileEditorPills"
 import { EditorContainer } from "./editorContainer"
-
-export const FileMainRightSide = ()=>{
+import { For } from "solid-js"
+export const FileMainRightSide = () => {
+    function getFiles() {
+        return files.filter((e) => e.opened == true)
+    }
     return (
-        <div class="ml-3 flex-1">            
-            <div class="flex flex-wrap flex-col h-[65px] basis-0 overflow-x-scroll scrollbar-thin scrollbar-thumb-black scrollbar-track-gray-200  scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
-                <FileEditorPills active={true} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
-                <FileEditorPills active={false} filename="test.md"/>
+        <div class="ml-3 flex-1">
+            <div class="flex flex-wrap content-start flex-col align h-[65px] basis-0 overflow-x-scroll scrollbar-thin scrollbar-thumb-black scrollbar-track-gray-200  scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
+                <For each={getFiles()}>{(file: FileStored, i) =>
+                    <FileEditorPills {...file} />
+                }</For>
+
             </div>
             <div class="flex-wrap pt-3">
-                <EditorContainer/>
+                <EditorContainer />
             </div>
         </div>
     )
