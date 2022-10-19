431fa5fdec54096a2691312fb103c32ed2309859
Ruggero Panzeri <ruggero.panzeri95@gmail.com>
2022-10-19 15:18:36 +0200

    changed editor pills and added open close

--- src/components/FileEditorPills.tsx
+++ src/components/FileEditorPills.tsx
@@ -1,16 +1,22 @@
 import { Component } from "solid-js"
+import { fileStore } from "../app/files/files.store"
 import { ButtonIcon, ButtonIconDimension } from "./ButtonIcon"
 
-export const FileEditorPills:Component<{filename:string, active:boolean}> = (props)=>{
-
+export const FileEditorPills:Component<{name:string, active:boolean}> = (props)=>{
+    function setActive(){
+        fileStore.setActive(props.name)
+    }
+    function closeFile(){
+        fileStore.setOpened(props.name, false)
+    }
     return (
-        <div classList={{"border-b-4":props.active}} class="bg-white cursor-pointer border-black  rounded-[10px] mx-1 flex px-2 py-2">
+        <div onClick={()=>setActive()} classList={{"border-b-4":props.active}} class="bg-white flex-1 cursor-pointer border-black w-40 rounded-[10px] mx-1 flex px-2 py-2">
             <div class="p-auto m-auto">
-                {props.filename}
+                {props.name}
             </div>
 
             <div class="mx-3 ">            
-                <ButtonIcon dimension={ButtonIconDimension.SMALL} hoverColor="#000000" hoverBgColor="#ffffff" iconName="del" color="bg-black" iconColor="text-white" />
+                <ButtonIcon onClickFn={()=>closeFile()} dimension={ButtonIconDimension.SMALL} hoverColor="#000000" hoverBgColor="#ffffff" iconName="del" color="bg-black" iconColor="text-white" />
             </div>
         </div>
     )
